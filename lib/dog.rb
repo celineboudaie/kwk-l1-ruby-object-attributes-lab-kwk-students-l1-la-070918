55f34ac7471db75698ef346f662097b8d3c453cc
celineboudaie <cboudaie@gmail.com>
2018-07-12 22:29:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # dog.rb
+class Dog
+  def initialize(name,breed)
+    @name= name
+    @breed= breed
+  end
+  
+  def name=(new_name)
+    @name= "Fido"
+  end 
+  
+
+def breed= (new_breed)
+  @breed= "Beagle"
+end 
+
+e

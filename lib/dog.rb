c30417f0c0fcac81e0afb1e1770a8bbd5c2b6fdd
celineboudaie <cboudaie@gmail.com>
2018-07-12 22:44:52 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,7 @@ class Dog
   # end
   
   def name
-    @name= name
+    @name
   end 
   
     def name=(name)
@@ -14,7 +14,7 @@ class Dog
   end
 
 def breed
-  @breed= breed
+  @breed
 end 
 
 def breed= (breed)
